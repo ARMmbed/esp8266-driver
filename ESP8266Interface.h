1b7b782dcef403a2d316fe7218eb4d0be64c0685
Veijo Pesonen <veijo.pesonen@arm.com>
2018-11-08 10:24:26 +0200

    Fixes signess bug

--- ESP8266Interface.h
+++ ESP8266Interface.h
@@ -354,7 +354,7 @@ private:
     // Drivers's socket info
     struct _sock_info {
         bool open;
-        uint16_t sport;
+        int16_t sport;
     };
     struct _sock_info _sock_i[ESP8266_SOCKET_COUNT];
 
